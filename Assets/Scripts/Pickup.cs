613b057f9a65aafa6145a073ed03198b4eb95e71
The Lame Brain <thelamebrain@gmail.com>
2021-09-08 06:43:59 -0700

    Fixed weapon/armor type insanity

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -7,11 +7,7 @@ public class Pickup : MonoBehaviour
     public enum type 
     { food, arrow, gold, bleed_pot, heal_pot, magic_pot, poison_pot, 
       gem_of_sight, ritual_bone, ritual_skull, ritual_book, ritual_candle,
-      amulet1, amulet2, armor_heavy1, armor_light1, axe1, axe2, bow1, bow2, dagger1, dagger2, robe1, shield1, shield2, staff1, staff2, sword1, sword2,
-      amulet3, amulet4, armor_heavy2, armor_light2, axe3, axe4, bow3, bow4, dagger3, dagger4, robe2, shield3, shield4, staff3, staff4, sword3, sword4,
-      amulet5, amulet6, armor_heavy3, armor_light3, axe5, axe6, bow5, bow6, dagger5, dagger6, robe3, shield5, shield6, staff5, staff6, sword5, sword6,
-      amulet7, amulet8, armor_heavy4, armor_light4, axe7, axe8, bow7, bow8, dagger7, dagger8, robe4, shield7, shield8, staff7, staff8, sword7, sword8
-    }
+      amulet, armor_heavy, armor_light, axe, bow, dagger, robe, shield, staff, sword}
 
     public type itemType;
 
